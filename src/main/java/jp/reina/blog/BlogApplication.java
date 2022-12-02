429212f32fe0b6e00f47aeba6729ff9d8455717a
Rena <lixunsam@gmail.com>
2022-12-02 16:08:16 +0800

    scan

--- src/main/java/jp/reina/blog/BlogApplication.java
+++ src/main/java/jp/reina/blog/BlogApplication.java
@@ -1,9 +1,11 @@
 package jp.reina.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("jp.reina.blog.mapper")
 public class BlogApplication {
 
 	public static void main(String[] args) {
